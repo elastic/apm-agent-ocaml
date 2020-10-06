11152b8c8fe8fb5b57a91965abbf13f76c1ed027
Adam Ringwood <adam@nexadmin.com>
2020-10-06 13:55:36 -0500

    child transactions are cool.

--- src/trace.ml
+++ src/trace.ml
@@ -18,7 +18,7 @@ let make_uuid =
   let state = Random.State.make_self_init () in
   Uuidm.v4_gen state
 
-let make_id x = make_uuid x |> Uuidm.to_string
+let make_id () = make_uuid () |> Uuidm.to_string
 
 let add_transaction_id (trace : t) uuid =
   { trace with transaction_id = Some (Uuidm.to_string uuid) }
@@ -36,12 +36,17 @@ let add_transaction_id_if_missing (trace : t) : t =
 
 let of_headers headers =
   let trace_id = match Cohttp.Header.get headers ecs_trace_id_header with
-  | Some id -> id 
-  | None -> make_id () 
+  | Some id -> id
+  | None -> make_id ()
   in
   let transaction_id = Cohttp.Header.get headers ecs_transaction_id_header in
   let parent_id = None in
   { trace_id; transaction_id; parent_id }
+
+let new_child_transaction (trace : t) =
+  let parent_id = trace.transaction_id in
+  let transaction_id = make_id () in
+  { trace with transaction_id; parent_id; }
 
 let to_header_list (trace : t) =
   List.filter_map
@@ -54,3 +59,13 @@ let to_header_list (trace : t) =
 let to_headers trace = Cohttp.Header.of_list (to_header_list trace)
 let add_to_headers headers trace =
   Cohttp.Header.add_list headers (to_header_list trace)
+
+let add_to_tags tags trace =
+  let trace_id : t option =
+    Option.map (fun id -> id) trace.trace_id
+  in
+  let transaction_id : t option =
+    Option.map (fun id -> id) trace.transaction_id
+  in
+  let ids = List.filter_map Fun.id [ trace_id; transaction_id ] in
+  Ecs.add_tags [ Ecs.trace ids ] tags
