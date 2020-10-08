54ec00bb36dc7210668a592e98b8bd82d404a2bd
Adam Ringwood <adam@nexadmin.com>
2020-10-08 08:15:09 -0500

    adding url converter.

--- src/http.ml
+++ src/http.ml
@@ -1,19 +1,27 @@
 type response = {
   status_code : int;
   transfer_size : int;
-} [@@deriving to_yojson]
+} [@@deriving to_yojson, make]
 
 type url = {
-  raw : string;
-  protocol : string;
-  full : string;
-  hostname : string;
-  port : int;
-  pathname : string;
-} [@@deriving to_yojson]
+  protocol : string option;
+  full : string option;
+  hostname : string option;
+  port : int option;
+  pathname : string option;
+} [@@deriving to_yojson, make]
 
 type request = {
   meth : string;
   url : url;
   http_version : string;
-} [@@deriving to_yojson]
+} [@@deriving to_yojson, make]
+
+let of_uri u =
+  let protocol = Uri.scheme u in
+  let full = Uri.to_string u in
+  let hostname = Uri.host u in
+  let port = Uri.port u in
+  let pathname = Uri.path u in
+  make ~protocol ~full ~hostname ~port ~pathname
+
