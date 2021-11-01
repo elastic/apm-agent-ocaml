d703ffb9f540effc3d3dc5a8133ee6d98eb13bcd
Anurag Soni <anurag@sonianurag.com>
2021-11-01 12:29:14 -0400

    hide ptime from public api

--- core/timestamp.ml
+++ core/timestamp.ml
@@ -1,5 +1,6 @@
 type t = int [@@deriving yojson_of]
 
-let of_time t =
+let now () =
+  let t = Ptime_clock.now () in
   let seconds = Ptime.to_float_s t in
   Float.to_int (seconds *. 1000. *. 1000.)
