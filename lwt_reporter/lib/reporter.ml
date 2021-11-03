5a561f69f5ce88834b360b5501d3bbe5d54280a0
Anurag Soni <anurag@sonianurag.com>
2021-11-03 15:31:10 -0400

    dont append metadata twice (#32)

--- lwt_reporter/lib/reporter.ml
+++ lwt_reporter/lib/reporter.ml
@@ -15,8 +15,7 @@ let make_headers t =
   Cohttp.Header.add_authorization (Cohttp.Header.of_list headers) auth
 ;;
 
-let make_body t events =
-  let events = Elastic_apm_core.Request.Metadata t.metadata :: events in
+let make_body events =
   let jsons =
     List.map
       (fun e -> Yojson.Safe.to_string (Elastic_apm_core.Request.yojson_of_t e))
@@ -28,7 +27,7 @@ let make_body t events =
 let send_events t headers events =
   Logs.info (fun m -> m "Sending events");
   let uri = Uri.with_path t.apm_server "/intake/v2/events" in
-  let body = Cohttp_lwt.Body.of_string (make_body t events) in
+  let body = Cohttp_lwt.Body.of_string (make_body events) in
   Cohttp_lwt_unix.Client.post ~headers ~body uri >>= fun (resp, body) ->
   Logs.info (fun m -> m "Response: %a" Cohttp.Response.pp_hum resp);
   Cohttp_lwt.Body.to_string body >|= fun body ->
