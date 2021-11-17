a7af5718e13a06ac27e452453d2f67244a1a6ae9
Anurag Soni <anurag@sonianurag.com>
2021-11-17 09:51:35 -0500

    Use the `main` entrypoint from dune-configurator (#10)

--- codegen/system_info.ml
+++ codegen/system_info.ml
@@ -10,9 +10,10 @@ let optional_var name var =
 ;;
 
 let () =
-  let configurator = C.create "elastic-apm" in
-  let arch = C.ocaml_config_var configurator "architecture" in
-  let system = C.ocaml_config_var configurator "system" in
-  print_endline (optional_var "architecture" arch);
-  print_endline (optional_var "platform" system)
+  C.main ~name:"elastic-apm.codegen" (fun configurator ->
+      let arch = C.ocaml_config_var configurator "architecture" in
+      let system = C.ocaml_config_var configurator "system" in
+      print_endline (optional_var "architecture" arch);
+      print_endline (optional_var "platform" system)
+  )
 ;;
