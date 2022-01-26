6d020c5eaaf38de2e5cfa250f0c76a0c390cc2e5
Anurag Soni <anurag@sonianurag.com>
2022-01-26 11:46:05 -0500

    Make logs less noisy for lwt reporter (#15)
    * Make logs less noisy for lwt reporter
    
    Also use a custom source which can be used to control the logs setup for
    the lwt reporter
    
    * Add log source to public api

--- lwt_reporter/lib/reporter.ml
+++ lwt_reporter/lib/reporter.ml
@@ -1,5 +1,9 @@
 open Lwt.Infix
 
+let log_source = Logs.Src.create "elastic_apm.lwt_reporter"
+
+module Log = (val Logs.src_log log_source)
+
 module Host = struct
   type t = {
     server : Uri.t;
@@ -45,14 +49,14 @@ let make_body events =
 ;;
 
 let send_events t headers events =
-  Logs.info (fun m -> m "Sending events");
+  Log.debug (fun m -> m "Sending events");
   let uri = Uri.with_path t.host.server "/intake/v2/events" in
   let body = Cohttp_lwt.Body.of_string (make_body events) in
   Cohttp_lwt_unix.Client.post ?ctx:t.cohttp_ctx ~headers ~body uri
   >>= fun (resp, body) ->
-  Logs.info (fun m -> m "Response: %a" Cohttp.Response.pp_hum resp);
+  Log.debug (fun m -> m "Response: %a" Cohttp.Response.pp_hum resp);
   Cohttp_lwt.Body.to_string body >|= fun body ->
-  Logs.info (fun m -> m "Body %s" body)
+  Log.debug (fun m -> m "Body %s" body)
 ;;
 
 let start_reporter t =
@@ -71,11 +75,11 @@ let start_reporter t =
   Lwt.async (fun () ->
       Lwt.catch
         (fun () ->
-          Logs.info (fun m -> m "starting loop");
+          Log.debug (fun m -> m "starting loop");
           loop ()
         )
         (fun exn ->
-          Logs.err (fun m ->
+          Log.err (fun m ->
               m "Exception in the reporter loop: %S" (Printexc.to_string exn)
           );
           loop ()
@@ -100,6 +104,6 @@ let create ?cohttp_ctx ?(max_messages_per_batch = 20) host metadata =
 ;;
 
 let push t event =
-  Logs.info (fun m -> m "Pushing events");
+  Log.debug (fun m -> m "Pushing events");
   t.push (Some event)
 ;;
